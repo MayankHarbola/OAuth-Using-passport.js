489bd24d9cf842d693b1f701cdd202586630cf1f
Mayank Harbola <36039776+MayankHarbola@users.noreply.github.com>
2019-06-01 05:27:14 +0530

    Update key.js

--- config/key.js
+++ config/key.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     google:{
-        clientID: '850799622536-f8ef3ethu73dulpo85himt642jg3sh49.apps.googleusercontent.com',
-        clientSecret: 'EG6XstEXZACeugaqunFQVa7Y'
+        clientID: '850799622536-1t9ght0lfd1bqcmdgfi7ofted4l3bf04.apps.googleusercontent.com',
+        clientSecret: 'kTWDdAJAWPsFw7ZKVLtdK5T4'
     },
     mongodb:{
         mongodbURI: 'mongodb+srv://admin:admin@shopdb-ykzx1.mongodb.net/auth?retryWrites=true&w=majority'
